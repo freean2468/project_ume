010e09badb3e74da2f66936a041c1505521d12a5
sensebecommon <freean2468@gmail.com>
2020-06-20 22:46:13 +0900

    v.0.3.1(Nav structure is undergoing)

--- src/client/nav/Nav.js
+++ src/client/nav/Nav.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './nav.css';
 import search from '../../../public/search.svg';
-import Category from './Category';
 
 class MenuButton extends Component {
   render() {
@@ -18,40 +17,152 @@ class Menu extends Component {
     super(props);
 
     this.state = {
-      listOfEnWord : null
+      search:'',
+      searchRes:{},
+      isActive:false,
+      isResSelected:false,
+      resList:[],
+      isResReceived:false
     }
 
-    this.getListOfEnglish();
+    this.optionRef = React.createRef();
+
+    this.handleOnMouseLeaveMenu = this.handleOnMouseLeaveMenu.bind(this);
+    this.handleOnClickRes = this.handleOnClickRes.bind(this);
   }
 
-  getListOfEnglish () {
-    fetch('/api/getListOfEnglish')
-      .then(res => res.json())
-      .then(json => this.setState({listOfEnWord:json.list}))
+  handleOnMouseLeaveMenu(e) {
+    this.props.handleMouseEnter(e);
   }
+
+  handleOnClickRes(e, key) {
+    this.setState({
+      isResSelected:!this.state.isResSelected,
+      isResReceived:false
+    });
+    fetch(`/api/search?id=${this.state.searchRes[key]}`)
+      .then(res => res.json())
+      .then(res => {
+          this.setState({
+            resList:res.links,
+            isResReceived:true
+          });
+      })
+  }
+
+  handleOnChange(value) {
+    let option = parseInt(this.optionRef.current.value), filter;
+    if (value === ''){
+      this.setState({searchRes:{}});
+    }
+    
+    switch(option) {
+      case 0: // USAGE
+        filter = /^['a-zA-Z-\ ]+$/;
+        if (value === '' || filter.test(value)) {
+          this.setState({search: value})
+
+          if (value !== '') {
+            fetch(`/api/preSearch?option=${option}&search=${value}`)
+              .then(res => res.json())
+              .then(res => { this.setState({searchRes:res}); })
+          }
+        }
+        break;
+      case 1: // KOR
+
+        break;
+      case 2: // GAME
+
+        break;
+    }
+  } 
 
   render() {
-    let visibility='hide';
+    let visibility = 'hide';
+    if (this.props.menuVisibility || this.state.isActive || this.state.isResSelected)
+      visibility = 'show'
+
+    let leftWidth = '100%';
+    let rightWidth = '0';
+    if (this.state.isResReceived) {
+      leftWidth = '25%';
+      rightWidth = '75%';
+    }
+
     return (
-      <>
-        <div id="flyoutMenu" className={visibility}
-          onMouseLeave={this.handleOnMouseLeaveMenu}>
+      <div id="flyoutMenu" className={visibility} onMouseLeave={this.handleOnMouseLeaveMenu}
+        onMouseDown={(e)=>this.setState({isActive:!this.state.isActive})}
+      >
+        <div className="Request" 
+          style={{height:`${this.props.height}px`}}
+        >
+          <select className="Options" defaultValue={0} ref={this.optionRef}>
+            <option value={0}>영어</option>
+            <option value={1}>한글</option>
+            <option value={2}>게임</option>
+          </select>
+          { (this.optionRef.current !== null && this.optionRef.current.value == 0) &&
+              <input className="Search"
+                  value={this.state.search}
+                  placeholder="english"
+                  onChange={(e) => this.handleOnChange(e.target.value)}
+              />
+          }
         </div>
-      </>
+        <div className="Response">
+          <div className="ResLeft" 
+            style={{
+              top:`${this.props.height}px`,
+              width:`${leftWidth}`
+            }}
+          >
+            {
+              Object.keys(this.state.searchRes).map((key, idx) => 
+                <div className="ResLeftItem" key={idx} onClick={(e)=>this.handleOnClickRes(e, key)}>
+                  {key}
+                </div>)
+            }
+          </div>
+          <div className="ResRight" 
+            style={{
+              top:`${this.props.height}px`,
+              left:`${leftWidth}`,
+              width:`${rightWidth}`
+            }}
+          >
+            {
+              this.state.resList.map((item, idx) =>
+                <div className="ResRightItem" key={idx} onClick={(e)=>console.log(item)}>
+                  ct:{item.ct} lt:{item.lt}
+                  <br></br>
+                  <img src={`https://img.youtube.com/vi/${item.link}/0.jpg`}/>
+                </div>
+              )
+            }
+          </div>
+        </div>
+      </div>
     );
   }
 }
 
 class MenuContainer extends Component {
-  constructor(props, context) {
-    super(props, context);
+  constructor(props) {
+    super(props);
    
     this.state = {
-      visible: false
+      visible: false,
+      height: 0
     };
-   
+
+    this.containerRef = React.createRef();
     this.toggleMenu = this.toggleMenu.bind(this);
     this.handleMouseEnter = this.handleMouseEnter.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({height:this.containerRef.current.offsetHeight});
   }
 
   handleMouseEnter(e) {
@@ -67,19 +178,18 @@ class MenuContainer extends Component {
   
   render() {
     return (
-      <div className="MenuContainer">
+      <div className="MenuContainer" ref={this.containerRef}>
         <MenuButton handleMouseEnter={this.handleMouseEnter}/>
         <Menu handleMouseEnter={this.handleMouseEnter}
-          menuVisibility={this.state.visible}/>
+          menuVisibility={this.state.visible}
+          height={this.state.height}
+        />
       </div>
     );
   }
 }
 
 export default class Nav extends Component {
-  componentDidMount() {
-
-  }
 
   render() {
     return (
--- src/server/index.js
+++ src/server/index.js
@@ -9,35 +9,105 @@ const {MongoClient} = require('mongodb');
 
 const PORT = process.env.PORT || 8090;
 
-const DATABASE_NAME = "sensebe_dictionary"
+const DATABASE_NAME = "sensebe_dictionary";
 const VIDEO_ARCHIVE_PATH = "collections/SB_VIDEO"
-const VIDEO_COLLECTION = "SB_VIDEO"
+const VIDEO_COLLECTION = "SB_VIDEO";
+const WORD_COLLECTION = "SB_WORD";
 
 const PASSWORD = fs.readFileSync("./pw.txt", "utf8")
 
-var listOfWordJson = JSON.parse(fs.readFileSync("./list_word.json", "utf8"));
+const LIST_WORD = "list_word.json"
 
-var listOfWord = [];
-
-initializeLists();
+var listWordJson = JSON.parse(fs.readFileSync(LIST_WORD, "utf8"));
 
 app.use(express.static("dist"));
 
-function getListOfEnglish(req, res) {
-    res.json({ list:listOfWord })
+function preSearch(req, res) {
+    const query = req.query;
+    const option = query.option;
+    const search = query.search;
+    let result = {}
+
+    // gotta change the algorithm later
+    switch(option) {
+        case '0': // Eng
+            for (let key in listWordJson) {
+                if (key.includes(search)) {
+                    result[key] = listWordJson[key]    
+                }
+                if (Object.keys(result).length >= 10) {
+                    break;
+                }
+            }
+            break;
+        case '1': // Kor
+
+            break;
+        case '2': // game
+
+            break;
+    }
+
+    if (Object.keys(result).length >= 2) {
+        result = sortObject(result);
+    }
+
+    function sortObject(o) {
+        var sorted = {},
+        key, a = [];
+    
+        for (key in o) {
+            if (o.hasOwnProperty(key)) {
+                    a.push(key);
+            }
+        }
+    
+        a.sort();
+
+        for (key = 0; key < a.length; key++) {
+            sorted[a[key]] = o[a[key]];
+        }
+        return sorted;
+    }
+
+    res.json(result);
+}
+
+async function search(req, res) {
+    const uri = `mongodb+srv://sensebe:${PASSWORD}@agjakmdb-j9ghj.azure.mongodb.net/test`
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    const query = req.query;
+    const id = query.id;
+
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        
+        const result = await client.db(DATABASE_NAME).collection(WORD_COLLECTION).findOne({ _id: parseInt(id) });
+
+        if (result) {
+            return res.json(result)
+        } else {
+            throw result
+        }
+    } catch (e) {
+        console.error(e)
+        res.json({res:e})
+    } finally {
+        await client.close()
+    }
 }
 
 async function getVideo(req, res) {
     const uri = `mongodb+srv://sensebe:${PASSWORD}@agjakmdb-j9ghj.azure.mongodb.net/test`
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    const id = req.query.id
 
     try {
         // Connect to the MongoDB cluster
         await client.connect()
-
-        let id = req.query.id
         
-        result = await client.db(DATABASE_NAME).collection(VIDEO_COLLECTION).findOne({ _id: id });
+        const result = await client.db(DATABASE_NAME).collection(VIDEO_COLLECTION).findOne({ _id: id });
             
         if (result) {
             return res.json(result)
@@ -52,7 +122,8 @@ async function getVideo(req, res) {
     }
 }
 
-app.get('/api/getListOfEnglish', (req, res) => getListOfEnglish(req, res));
+app.get('/api/preSearch', (req, res) => preSearch(req, res));
+app.get('/api/search', (req, res) => search(req, res));
 app.get('/api/getVideo', (req, res) => getVideo(req, res));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
@@ -72,10 +143,4 @@ async function replaceListing(client, listing, collection) {
     
     // console.log(`${result.matchedCount} document(s) matched the query criteria.`);
     console.log(`_id : ${listing['_id']}, for "${listing["link"]}" replaced : matchedCount(${result.matchedCount}), modiefiedCount(${result.modifiedCount})`);
-}
-
-function initializeLists() {
-    for (let key in listOfWordJson) {
-        listOfWord.push(key);
-    }
 }
